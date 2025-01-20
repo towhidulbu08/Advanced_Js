6b7e0578bd73f99c63bcc9e9fdec31ac84366b12
towhidulbu08 <towhidulislam26121997@gmail.com>
2025-01-20 11:34:42 +0600

    first commit

---
+++ symbols/symbol.js
@@ -0,0 +1,7 @@
+let symbol1 = Symbol("I am Symbol One"); //symbol with description
+let symbol2 = Symbol("I am Symbol Two");
+
+let title = "Tamim";
+console.log(title);
+console.log(symbol1);
+console.log(symbol2);
