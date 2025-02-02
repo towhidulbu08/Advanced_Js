bac895c12c149b6a3c07148b572e55dc587ec36c
towhidulbu08 <towhidulislam26121997@gmail.com>
2025-02-02 12:44:06 +0600

    Static methods(built in methods) is completed

--- symbols/symbol.js
+++ symbols/symbol.js
@@ -1,12 +1,50 @@
-let includes = Symbol("my own array includes method");
+//let title = "js";
+//step-01: let title= new String("js")
+//step-02: convert parameter  into Regexp object
+//step-03: if the implementation of Symbol.search() method has present in  Regexp object
 
-Array.prototype[includes] = () => {
-  console.log("This is My array includes function");
-};
+// console.dir(RegExp);
 
-let myArray = [1, 2, 3];
+// console.dir(String);
+// console.log(title.search("s"));
 
-console.log(myArray["includes"](2));
-console.log(myArray[includes](2));
+class Product {
+  constructor(title) {
+    this.title = title;
+  }
 
-// Object.keys() => return an array of keys of that object
+  //implement search method
+  [Symbol.search](string) {
+    console.log(string);
+    return string.indexOf(this.title) >= 0 ? "Found" : "Not Found";
+  }
+}
+
+let laptop = new Product("laptop");
+// console.log(laptop);
+// console.log("JavaScript".search("Script"));
+
+// console.log("Hp aptop".search(laptop));
+
+function myFunc() {
+  const PVT = Symbol.for("my private value");
+
+  return {
+    modify(obj) {
+      obj[PVT] = true;
+    },
+    output(obj) {
+      return obj[PVT];
+    },
+  };
+}
+
+let value = myFunc();
+const obj = { a: 1, b: 2 };
+value.modify(obj);
+console.log(obj[Symbol.for("my private value")]);
+
+delete obj[Symbol.for("my private value")];
+
+console.log(obj);
+//console.log(value.output(obj));
